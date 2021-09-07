98060376f9a0ab78c8478815a3a05b108093bc73
Jangboo Lee <github@jangboolee.com>
2021-09-07 14:45:53 +0000

    Update digital_root.py

--- digital_root.py
+++ digital_root.py
@@ -14,7 +14,7 @@ def digital_root(n):
     input_string = str(n)
     
     # Find the first digital root
-    first_sum = sum_all_digits(str(n))
+    first_sum = sum_all_digits(input_string)
     
     # Return the first sum if the digital root is only one digit
     if len(str(first_sum)) <= 1:
